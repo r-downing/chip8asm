1617a1246f50dc6d030e0ecf68b46687e3d04295
Ryan Downing <26515643+r-downing@users.noreply.github.com>
2023-07-23 20:53:42 -0400

    fix tests script

--- scripts.py
+++ scripts.py
@@ -4,6 +4,6 @@ import subprocess
 def test():
     """
     Run all unittests. Equivalent to:
-    `poetry run python -u -m unittest discover`
+    `poetry run python -m unittest discover`
     """
     subprocess.run(["python", "-m", "unittest", "discover", "tests"])
